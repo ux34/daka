5c854d3deb86ae38662421403991e72edafb4957
“ux34” <umbrella34@qq.com>
2021-08-02 09:20:56 +0800

    perf:将重新登录代码移动到index.js中

--- request.js
+++ request.js
@@ -7,139 +7,121 @@
 
 const fetch = require("./node-fetch/lib");
 
-// 同步倒计时 秒
-const countdown = (second) => new Promise((resolve) => {
-  setTimeout(() => resolve(), 1000 * second)
-});
-
 // 模拟登录
-let 重新登录次数 = 2;
-async function login(info) {
-  try {
-    console.log("提交登录表单");
-    let res = await fetch("https://api.weishao.com.cn/login?source=%2Foauth%2Fauthorize%3Fclient_id%3DpqZ3wGM07i8R9mR3%26redirect_uri%3Dhttps%253A%252F%252Fyq.weishao.com.cn%252Fcheck%252Fquestionnaire%26response_type%3Dcode%26scope%3Dbase_api%26state%3Druijie", {
-      "headers": {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
-        "cache-control": "no-cache",
-        "content-type": "application/x-www-form-urlencoded",
-        "pragma": "no-cache",
-        "sec-ch-ua": "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "same-origin",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-      },
-      "referrerPolicy": "no-referrer",
-      "body": `schoolcode=xmut&username=${info['学号']}&password=${info['密码']}&verifyValue=&verifyKey=${info['学号']}_xmut&ssokey=`,
-      "method": "POST",
-      "mode": "cors",
-      redirect: 'manual' // 手动重定向，才可以拿到location
-    });
+async function login (info) {
+  console.log("提交登录表单");
+  let res = await fetch("https://api.weishao.com.cn/login?source=%2Foauth%2Fauthorize%3Fclient_id%3DpqZ3wGM07i8R9mR3%26redirect_uri%3Dhttps%253A%252F%252Fyq.weishao.com.cn%252Fcheck%252Fquestionnaire%26response_type%3Dcode%26scope%3Dbase_api%26state%3Druijie", {
+    "headers": {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+      "cache-control": "no-cache",
+      "content-type": "application/x-www-form-urlencoded",
+      "pragma": "no-cache",
+      "sec-ch-ua": "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "same-origin",
+      "sec-fetch-user": "?1",
+      "upgrade-insecure-requests": "1",
+    },
+    "referrerPolicy": "no-referrer",
+    "body": `schoolcode=xmut&username=${info['学号']}&password=${info['密码']}&verifyValue=&verifyKey=${info['学号']}_xmut&ssokey=`,
+    "method": "POST",
+    "mode": "cors",
+    redirect: 'manual' // 手动重定向，才可以拿到location
+  });
 
-    // 返回oauth2cookie
-    let oauth2cookie = res.headers.get("set-cookie").split(";")[0];
-    // console.log(oauth2cookie);
-    let url = res.headers.get("location"); // 获得重定向地址
-    console.log("\n第1次重定向");
-    // console.log(url);
+  // 返回oauth2cookie
+  let oauth2cookie = res.headers.get("set-cookie").split(";")[0];
+  // console.log(oauth2cookie);
+  let url = res.headers.get("location"); // 获得重定向地址
+  console.log("\n第1次重定向");
+  // console.log(url);
 
 
-    // 第一次 重定向
-    res = await fetch(url, {
-      "headers": {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
-        "cache-control": "no-cache",
-        "pragma": "no-cache",
-        "sec-ch-ua": "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "same-site",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-      },
-      "referrerPolicy": "no-referrer",
-      "body": null,
-      "method": "GET",
-      "mode": "cors",
-      redirect: 'manual'
-    })
-    url = res.headers.get("location")
-    console.log("\n第2次重定向");
-    // console.log(url);
+  // 第一次 重定向
+  res = await fetch(url, {
+    "headers": {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+      "cache-control": "no-cache",
+      "pragma": "no-cache",
+      "sec-ch-ua": "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "same-site",
+      "sec-fetch-user": "?1",
+      "upgrade-insecure-requests": "1",
+    },
+    "referrerPolicy": "no-referrer",
+    "body": null,
+    "method": "GET",
+    "mode": "cors",
+    redirect: 'manual'
+  })
+  url = res.headers.get("location")
+  console.log("\n第2次重定向");
+  // console.log(url);
 
 
-    // 第二次 重定向
-    res = await fetch(url, {
-      "headers": {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
-        "cache-control": "no-cache",
-        "pragma": "no-cache",
-        "sec-ch-ua": "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "same-site",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-        "cookie": oauth2cookie
-      },
-      "referrerPolicy": "no-referrer",
-      "body": null,
-      "method": "GET",
-      "mode": "cors",
-      redirect: 'manual'
-    })
-    url = res.headers.get("location")
-    console.log("\n第3次重定向");
-    // console.log(url);
+  // 第二次 重定向
+  res = await fetch(url, {
+    "headers": {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+      "cache-control": "no-cache",
+      "pragma": "no-cache",
+      "sec-ch-ua": "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "same-site",
+      "sec-fetch-user": "?1",
+      "upgrade-insecure-requests": "1",
+      "cookie": oauth2cookie
+    },
+    "referrerPolicy": "no-referrer",
+    "body": null,
+    "method": "GET",
+    "mode": "cors",
+    redirect: 'manual'
+  })
+  url = res.headers.get("location")
+  console.log("\n第3次重定向");
+  // console.log(url);
 
 
-    // 第三次 重定向
-    res = await fetch(url, {
-      "headers": {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
-        "cache-control": "no-cache",
-        "pragma": "no-cache",
-        "sec-ch-ua": "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "same-site",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1"
-      },
-      "referrerPolicy": "no-referrer",
-      "body": null,
-      "method": "GET",
-      "mode": "cors",
-      redirect: 'manual'
-    })
-    let cookie = res.headers.get("set-cookie").split(";")[0];
-    // console.log("真正的登录cookie:", cookie);
-    return cookie
-  } catch (error) {
-    console.log("登录失败：", error.message);
-    // 登录很容易错误，多登录几次试试
-    if(重新登录次数--){
-      console.log("3分钟后重新登录");
-      // 等待3分钟
-      await countdown(3 * 60);
-      login(info);
-    }
-    throw new Error('登录失败:' + error.message)
-  }
+  // 第三次 重定向
+  res = await fetch(url, {
+    "headers": {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+      "cache-control": "no-cache",
+      "pragma": "no-cache",
+      "sec-ch-ua": "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "same-site",
+      "sec-fetch-user": "?1",
+      "upgrade-insecure-requests": "1"
+    },
+    "referrerPolicy": "no-referrer",
+    "body": null,
+    "method": "GET",
+    "mode": "cors",
+    redirect: 'manual'
+  })
+  let cookie = res.headers.get("set-cookie").split(";")[0];
+  // console.log("真正的登录cookie:", cookie);
+  return cookie
 }
 
 
 // 获取用户信息
-async function getUserInfo(cookie) {
+async function getUserInfo (cookie) {
   let userInfo = await fetch("https://yq.weishao.com.cn/userInfo", {
     "headers": {
       "accept": "*/*",
@@ -170,7 +152,7 @@ async function getUserInfo(cookie) {
 
 
 // 模拟提交
-async function submit(cookie, body) {
+async function submit (cookie, body) {
   return await fetch("https://yq.weishao.com.cn/api/questionnaire/questionnaire/addMyAnswer", {
     "headers": {
       "accept": "*/*",
@@ -189,12 +171,12 @@ async function submit(cookie, body) {
     "method": "POST",
     "mode": "cors"
   })
-  .then(res => {
-    return res.json();
-  })
-  .then(json => {
-    return json['errmsg'];
-  })
+    .then(res => {
+      return res.json();
+    })
+    .then(json => {
+      return json['errmsg'];
+    })
 }
 
 module.exports = {
